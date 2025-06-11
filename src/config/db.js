fb65a7aed2a2bdc65712fcb4800b5cfdd7335a62
Markel Cortajarena <markelkortajarena@gmail.com>
2025-06-12 00:33:21 +0200

    Refactor MongoDB connection logic to use NODE_ENV for environment-specific URLs

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL);
+let mongo_url;
+
+switch (process.env.NODE_ENV) {
+    case 'DEVELOPMENT':
+        console.log('Connecting to MongoDB in DEVELOPMENT mode...');
+        mongo_url = process.env.MONGO_DEVELOPMENT_URL;
+        break;
+    case 'PRODUCTION':
+        console.log('Connecting to MongoDB in PRODUCTION mode...');
+        mongo_url = process.env.MONGO_PRODUCTION_URL;
+        break;
+    default:
+        throw new Error('NODE_ENV must be either DEVELOPMENT or PRODUCTION');
+}
+
+mongoose.connect(mongo_url);

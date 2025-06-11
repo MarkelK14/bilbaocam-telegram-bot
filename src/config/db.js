52acee4aef30a7e0357bc014e6a1256e13306f7b
Markel Cortajarena <markelkortajarena@gmail.com>
2025-06-12 01:43:42 +0200

    Refactor database connection to use a single MongoDB URI from environment variables

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,3 @@
 const mongoose = require('mongoose');
 
-let mongo_url;
-
-switch (process.env.NODE_ENV) {
-    case 'DEVELOPMENT':
-        console.log('Connecting to MongoDB in DEVELOPMENT mode...');
-        mongo_url = process.env.MONGO_DEVELOPMENT_URL;
-        break;
-    case 'PRODUCTION':
-        console.log('Connecting to MongoDB in PRODUCTION mode...');
-        mongo_url = process.env.MONGO_PRODUCTION_URL;
-        break;
-    default:
-        throw new Error('NODE_ENV must be either DEVELOPMENT or PRODUCTION');
-}
-
-mongoose.connect(mongo_url);
+mongoose.connect(process.en.MONGO_DB_URI);
